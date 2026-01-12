605a0b31145a4c48ee9f66ccd59ab6ed3cf178d0
Sedef Bozkurt <sedefbozkurt.4379@gmail.com>
2026-01-12 19:13:29 +0300

    Initial project setup with folder structure

---
+++ MiniJournal/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  MiniJournal
+//
+//  Created by Sedef Bozkurt on 12.01.2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ MiniJournal/MiniJournalApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiniJournalApp.swift
+//  MiniJournal
+//
+//  Created by Sedef Bozkurt on 12.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct MiniJournalApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
